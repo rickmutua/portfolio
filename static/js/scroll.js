f4a368d2906f4cc396791686b6e544d835b30803
Erick <rick.mutua12@gmail.com>
2018-02-11 16:44:31 +0300

    minor update

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -6,3 +6,8 @@ $('.smooth').on('click', function() {
 
     return false;
 });
+
+var http = require("http");
+setInterval(function() {
+    http.get("http://erick-mutua.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
